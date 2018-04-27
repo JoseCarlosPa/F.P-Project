46ec25d73d5f45939bb8a247b46d2280f963a52e
Josecarlospas <josecarlospasq@gmail.com>
2018-04-27 09:28:18 -0500

    Main en python,junto con sus Librerias

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 """/**
 *Author: Jose Carlos Pacheco Sanchez
-*Start Date: April 17 2018
+*Start Date: April 12 2018
 *Finish Date: April 27 2018
 *ID: A01702828
 *School: ITESM (Qro.Mexico)
@@ -12,7 +12,6 @@
 # Inicio de Libriras y docuemntos aparte
 
 import os
-import time
 import Libreria
 
 
@@ -26,17 +25,17 @@ def main():
         opcion = input("Ingresa La opcion que deseas: ")
         contador = 0  # Contador que nos sirve para lanzar un mensaje de ayuda a los 4 intentos de fallo
 
-        while (
-                    opcion >= 4 or opcion <= 0 or opcion % 1 != 0):  # Validacion de la respuesta ingresada si no se cumple entrara hasta que sea valido
+        while opcion >= 4 or opcion <= 0 or opcion % 1 != 0:
+            # Validacion de la respuesta ingresada si no se cumple entrara hasta que sea valido
             opcion = input("Porfavor ingresa un numero valido: ")
             contador = contador + 1  # por cada ciclo el contador aumentara en 1
 
             if contador >= 3:
                 # Despliegue de un mensaje cuando contador es mayor o igual a tres
-                print(
-                "\nLos rangos para poder ingresar a una opcion son de 1 al 4,\nno pueden ser caracteres, numeros negativos, "
-                "tener punto decimal o ser algun tipo de simbolo \n"
-                "debera ser  1,2,3 o 4\n")
+                print "\n\nLos rangos para poder ingresar a una opcion son de 1 al 4,"
+                print "no pueden ser caracteres, numeros negativos, "
+                print "tener punto decimal o ser algun tipo de simbolo "
+                print "debera ser  1,2,3 o 4\n\n"
                 contador = 0
 
         # En todos los casos se agrego una variable llamada espera, tiene como uso copiar el system pause en c
@@ -46,11 +45,6 @@ def main():
             os.system("cls")
             print("Usted entro a programa en Python: ")
             Libreria.cargando()
-
-
-
-
-
         elif opcion == 2:
             # Programa numero dos
             os.system("cls")

0724f6f44b455d374fc00a60febd2b940fed0b38
Josecarlospas <josecarlospasq@gmail.com>
2018-04-27 11:35:58 -0500

    Cambios ya hechos

--- Libreria.py
+++ Libreria.py
@@ -1,14 +1,20 @@
 import time
 
 
+# Seccion de Menus y partes Graficas de bajo nivel
+
 def menu_principal():
-    print ("\n|----------------------------|")
-    print ("|            MENU            |")
-    print ("|----------------------------|")
-    print ("|1. Programa en python-1     |")
-    print ("|2. Mostrar Archivo de C P1  |")
-    print ("|3. Salir                    |")
-    print ("|----------------------------|")
+    print ("\n|-----------------------------------|")
+    print ("|                MENU               |")
+    print ("|-----------------------------------|")
+    print ("|1. Programa en python 1            |")
+    print ("|2. Codigo en c                     |")
+    print ("|3. Ejecutar programa c             |")
+    print ("|4. Programa en python Laboratorio  |")
+    print ("|5. Codigo en c                     |")
+    print ("|6. Ejecutar programa c             |")
+    print ("|7. Salir                           |")
+    print ("|-----------------------------------|")
 
 
 def pantalla_carga():
@@ -20,3 +26,21 @@ def pantalla_carga():
     for i in range(0, 32):
         print'*',
         time.sleep(.025)
+
+
+def bienvenido():
+    print("------------------------------------------------------------------------------")
+    print"  ____    _ "
+    print" | __ )  (_)   ___   _ __   __   __   ___   _ __   (_)   __| |   ___    ___ "
+    print" |  _ \  | |  / _ \ | '_ \  \ \ / /  / _ \ | '_ \  | |  / _` |  / _ \  / __|"
+    print" | |_) | | | |  __/ | | | |  \ V /  |  __/ | | | | | | | (_| | | (_) | \__ \ "
+    print" |____/  |_|  \___| |_| |_|   \_/    \___| |_| |_| |_|  \__,_|  \___/  |___/"
+    print("------------------------------------------------------------------------------")
+
+
+# Fin de la seccion  de Menus y partes Graficas de bajo nivel, inicio de funciones y procedimientos
+
+def calcular_dias_que_tarda(altura,subida,bajada):
+    dias_que_tarda=0
+
+    return dias_que_tarda
--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """/**
 *Author: Jose Carlos Pacheco Sanchez
 *Start Date: April 12 2018
@@ -11,15 +12,18 @@
 
 # Inicio de Libriras y docuemntos aparte
 
-import os
+
+import os  # Libreria para usar funciones de windows, se cambia si fueran MAC
 import Libreria
+
 
 # Fin de las librerias
 
+
 def main():
     opcion = 0  # Iniciar opcion en 0 para que mientras no sea 5 se repita
-
-    while opcion != 5:
+    # Ejecutar hasta colocar la opcion 7 que manda terminar el programa
+    while opcion != 7:
         os.system("cls")
         Libreria.menu_principal()  # Mostrar menu
 
@@ -38,23 +42,37 @@ def main():
                 print "tener punto decimal o ser algun tipo de simbolo "
                 print "debera ser  1,2,3 o 4\n\n"
                 contador = 0
-
-        # En todos los casos se agrego una variable llamada espera, tiene como uso copiar el system pause en c
-
+        # Programa numero uno
         if opcion == 1:
-            # Programa numero uno
             os.system("cls")
             print('Usted entro a programa en Python: ')
             # LLamamos la funcion procedimiento de cargado que mostrara una animacion con asteriscos
             Libreria.pantalla_carga()
+            os.system("PAUSE")
+            os.system("cls")
+            # Inicia programa numero uno
+            print "\n Un caracol decidió subir a un árbol de 15 m de altura. "
+            print"Durante cada día tenía tiempo de subir 5 m; "
+            print" pero mientras dormía por la noche, bajaba 4 m. "
+            print"¿Al cabo de cuántos días llegará a la cima del árbol?\n "
+            print("En base estos datos genera un programa que calcule los dias dependiendo de los datos")
+            print "Ingresa los datos para calcular el tiempo estimdao:"
+
+            altura_arbol = input("Dame la altura del arbol")
+            metros_subidos = input("Dame los metros que sube por dia")
+            metros_baja = input("Dame los metros que baja por noche")
+            print ("Tarda " + Libreria.calcular_dias_que_tarda(altura_arbol, metros_subidos, metros_baja) + "Dias en subir el arbol")
+
+            os.system("PAUSE")
         elif opcion == 2:
             # Programa numero dos
             os.system("cls")
             print("Usted entro a Archivo en c")
-        elif opcion == 3:
+        # Opcion para salir o terminar el programa
+        elif opcion == 7:
             exit(1)
 
 
-print("Bienvenido al maravilloso mundo de python\n\n")
+Libreria.bienvenido()
 os.system("Pause")
 main()
